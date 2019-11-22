e6b96cefd861318f9b43ac32e87209b6b9122f31
Matthew Fischer <mattfisc@gmail.com>
2019-11-22 02:00:44 -0800

    update php lab 4

--- lab4.php
+++ lab4.php
@@ -2,12 +2,13 @@
     // task 1 read and open file
     $line = file("play.txt");
     // taske 2 select term
-    list($word, $name, $definition) = preg_split("/[t]/",trim($line[array_rand($line)]));
+    list($word, $name, $definition) = preg_split("/[\t]/",trim($line[array_rand($line)]));
     // task 3
 $json = array{
     "definition" => $definition,
     "word" => $word
-}
+};
     // task 4
     header("Contenttype:Application/json");
+    print(json_encode($json));
 ?>
