623931ca39cf4726df58f3fa019a3c30dd168e98
Matthew Fischer <mattfisc@gmail.com>
2019-11-22 02:07:05 -0800

    found the lost file, it is still not working.  exactly same as the teachers: ?:

---
+++ lab10ex3.php
@@ -0,0 +1,14 @@
+<?php
+    // task 1 read and open file
+    $line = file("play.txt");
+    // taske 2 select term
+    list($word, $name, $definition) = preg_split("/[\t]/",trim($line[array_rand($line)]));
+    // task 3
+$json = array{
+    "definition" => $definition,
+    "word" => $word
+};
+    // task 4
+    header("Contenttype:Application/json");
+    print(json_encode($json));
+?>
