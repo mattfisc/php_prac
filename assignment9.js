578831c1b4e32a96923fc689759bcd9c82b3b0d8
Matthew Fischer <mattfisc@gmail.com>
2019-11-26 15:31:45 -0800

    update php and js

--- assignment9.js
+++ assignment9.js
@@ -4,7 +4,19 @@ var option1 = document.getElementById("option1").innerHTML;
 var option2 = document.getElementById("option2").innerHTML;
 var option3 = document.getElementById("option3").innerHTML;
 
+var data = array();
+
 function get_select(){
     var select = document.getElementById("list").value;
-    console.log(select);
+    fetchList();
+}
+
+function readFile(){
+
+}
+
+function fetchList(){
+    var myRequest = new XMLHttpRequest();
+    myRequest.open("GET", "play.php", true);
+    myRequest.send();
 }
