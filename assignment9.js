f46d52d7e2a1528e668f55a3f2dd9dec4c9b6b32
Matthew Fischer <mattfisc@gmail.com>
2019-11-28 18:08:35 -0800

    assignment html, js

--- assignment9.js
+++ assignment9.js
@@ -1,20 +1,39 @@
-var question = document.getElementById("question").innerHTML;
+/*
+window.onload = function() {
+	//TASK: FETCH A TERM
+	fetchTerm();
+};
+*/
 
-var option1 = document.getElementById("option1").innerHTML;
-var option2 = document.getElementById("option2").innerHTML;
-var option3 = document.getElementById("option3").innerHTML;
-
-function get_select(){
-    var select = document.getElementById("list").value;
-    fetchList();
+function fetchTerm() {
+    var part = document.getElementById("part").value;  // select which culinary topic to quiz about
+    var myXMLRequest = new XMLHttpRequest();
+    myXMLRequest.onload = createPossibleAnswers;
+    myXMLRequest.open("GET", "word.php?part=" + part, true); // send which topic is being quizzed
+    myXMLRequest.send();
 }
 
-function readFile(){
+function createPossibleAnswers() {
+    //TASK 1: CLEAR OUT ANY PREVIOUS OUTPUT
+    document.getElementById("result").innerHTML = "";  // set the output to nothing
 
+    var json = JSON.parse(this.responseText);
+    //TASK 1: GET THE WORD
+    document.getElementById("word").innerHTML = "What answer best applies to the " + json.part + ":  " + json.word;
+
+    //TASK 2: GET THE MULTIPLE CHOICE ANSWERS
+    document.getElementById("choices").innerHTML = "";
+    for (var i = 0; i < json.choices.length; i++) {
+        var button = document.createElement("button");
+        button.innerHTML = json.choices[i].definition;
+        button.className = json.choices[i].correct ? "correct" : "incorrect";
+        button.onclick = showResult;
+        button.style.display = "block";
+        document.getElementById("choices").appendChild(button);
+    }
 }
 
-function fetchList(){
-    var myRequest = new XMLHttpRequest();
-    myRequest.open("GET", "play.php", true);
-    myRequest.send();
+function showResult() {
+    document.getElementById("result").innerHTML = "You are " + this.className;
+    //fetchTerm();
 }
