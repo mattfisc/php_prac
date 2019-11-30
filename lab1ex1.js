dd6bac2b3484effc27957519d48f9d365c62dd02
Matthew Fischer <mattfisc@gmail.com>
2019-11-30 00:00:21 -0800

    good progress on ex1 lab1

--- lab1ex1.js
+++ lab1ex1.js
@@ -1,5 +1,17 @@
 var data = document.getElementById("data").addEventListener('click',myFunc);
 
 function myFunc(){
-    
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET','play.tx',true);
+    xhr.onload = function(){
+        if(xhr.status== 200){
+            document.write(this.responseText);
+        }
+        else if(xhr.status == 404){
+            document.write("help file error");
+        }
+    }
+
+    xhr.send();
+
 }
