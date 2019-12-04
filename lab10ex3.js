baaf9f8e15e955f5766db37fc326e2bbbab53f72
Matthew Fischer <mattfisc@gmail.com>
2019-12-03 16:50:56 -0800

    lab 10 ex 3 from trish

--- lab10ex3.js
+++ lab10ex3.js
@@ -1,17 +1,14 @@
-// JS
-function fetchTerm() {
-
-    var myXMLRequest = new XMLHttpRequest();
-    myXMLRequest.open("GET", "lab10ex3.php", true);
-    myXMLRequest.send();
-  }
-  
-  function createRandomWordDefin() {
-    //TASK 1: PARSE THE RETURNED JSON OBJECT
+function fetchFact() {
+  var myXMLRequest = new XMLHttpRequest();
+  myXMLRequest.onload = createWordDefinition;
+  myXMLRequest.open("GET", "lab10c.php", true);
+  myXMLRequest.send();
+}
+ 
+function createWordDefinition(){
+  //TASK 1: PARSE THE RETURNED JSON OBJECT
   var json = JSON.parse(this.responseText);
-  
-    //TASK 2: GET THE DEFINITION AND WORD
-    document.getElementById("defin").innerHTML = json.definition;
-    document.getElementById("word").innerHTML = json.word;
-  }
-  
+  //TASK 2: GET THE DEFINITION AND WORD AND DISPLAY IT
+  document.getElementById("description").innerHTML = "This C++ fact concerns " + json.factType + ":";
+  document.getElementById("factStatement").innerHTML =  json.fact;
+}
--- lab10ex3.php
+++ lab10ex3.php
@@ -1,18 +1,31 @@
 <?php
-//TASK 1: OPEN AND READ THE FILE INTO ARRAY
-$line = file("play.txt");
-
-//TASK 2: RANDOMLY SELECT A TERM AND definition
-list($word, $name, $definition) = preg_split("/[\t]/", trim($line[array_rand($line)]));
-
-//TASK 3: CREATE A JSON OBJECT
-$json = array (
-  "definition" =>$definition,
-  "word" => $word
+//TASK 1: CREATE AN ARRAY OF ARRAYS CONTAINING A TYPE OF FACT AND THE FACT
+$array = array();
+$array[] = array("popularity", "C++ gained instant popularity in 1983, the year it was introduced.");
+$array[] = array("popularity", "C++ was the third most popular language in 2018.");
+$array[] = array("popularity", "Most of Adobe, and much of Amazon’s websites are written in C++.");
+$array[] = array("popularity", "C++ has remained in demand since 1983 because it is a powerful tool that is adaptable.");
+$array[] = array("characteristic", "C++ is an Object Oriented Programming Language (OOPL).");
+$array[] = array("characteristic", "C++ have huge Function Library.");
+$array[] = array("characteristic", "C++ is highly Flexible language with Versatility.");
+$array[] = array("characteristic", "C++ can be used for developing System Software viz., operating systems, compilers, editors and data bases.");
+$array[] = array("characteristic", "C++ is suitable for Development of Reusable Software.");
+ 
+ 
+//TASK 2: RANDOMLY SELECT AN ELEMENT FROM THE ARRAY
+$item = mt_rand(0,8);
+$factType = $array[$item][0];
+$fact = $array[$item][1];
+ 
+//TASK 3: PACK THE WORD AND DEFINITION INTO A JSON OBJECT
+$json = array(
+  "factType" => $factType,
+  "fact" => $fact
 );
-
-//TASK 4: RETURN THE JSON OBJECT 
-header("Contenttype:Application/json");
-print(json_encode($json));
-
+ 
+ 
+//TASK 4: RETURN THE JSON OBJECT TO THE JAVASCRIPT FUNCTION
+header("Content-type: application/json");
+print (json_encode($json));
 ?>
+ 
