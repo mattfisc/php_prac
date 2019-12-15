e0d92b3ecd2afa3322e95ad03cc672442e4a658c
Matthew Fischer <mattfisc@gmail.com>
2019-12-14 21:57:23 -0800

    php lab3

---
+++ lab3.js
@@ -0,0 +1,22 @@
+var data = document.getElementById("data").addEventListener('click',myFunc);
+
+function myFunc(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET','play.txt',true);
+    console.log(xhr);
+
+    xhr.onload = callFunc;
+    }
+
+    xhr.send();
+
+}
+
+function callFunc(){
+    console.log(xhr);
+    if(xhr.status== 200){
+        document.getElementById("display").innerHTML=xhr.responseText;
+    }
+    else{
+        document.write("help file error");
+    }
