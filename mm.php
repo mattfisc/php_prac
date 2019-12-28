38b053d7b8b87eca9bf72518bd7eeb6112c6734a
Matthew Fischer <mattfisc@gmail.com>
2019-12-28 02:20:02 -0800

    php form POST prac

---
+++ mm.php
@@ -0,0 +1,31 @@
+<?php
+
+$first = $_POST['first'];
+$last = $_POST['last'];
+$email = $_POST['email'];
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd'];
+
+$mysqli = new mysqli("localhost","root","","cs222");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+  }
+
+// TEST IF MEMBER EXISTS BY EMAIL
+$test = "SELECT * FROM `fitnesssignup` WHERE Email = '$email' ";
+
+
+// TEST
+$check = mysqli_query($mysqli,$test);
+
+// NUMBER OF ROWS OF DATA
+$num = mysqli_num_rows($check);
+
+if($num == 1){
+    echo "found same email";
+}
+// CREATE NEW MEMBER
+else{
+    
+}
+?>
