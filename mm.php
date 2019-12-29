0df5a921190a0665bd9fc1e9802fdb62e7aea6d7
Matthew Fischer <mattfisc@gmail.com>
2019-12-28 21:54:56 -0800

    working php POST form

--- mm.php
+++ mm.php
@@ -3,8 +3,10 @@
 $first = $_POST['first'];
 $last = $_POST['last'];
 $email = $_POST['email'];
-$uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
+
+
+
 
 $mysqli = new mysqli("localhost","root","","cs222");
 if ($mysqli->connect_errno) {
@@ -12,7 +14,7 @@ if ($mysqli->connect_errno) {
   }
 
 // TEST IF MEMBER EXISTS BY EMAIL
-$test = "SELECT * FROM `fitnesssignup` WHERE Email = '$email' ";
+$test = "SELECT * FROM `prac` WHERE Email = '$email' ";
 
 
 // TEST
@@ -21,11 +23,28 @@ $check = mysqli_query($mysqli,$test);
 // NUMBER OF ROWS OF DATA
 $num = mysqli_num_rows($check);
 
+// MEMBER EXISTS
 if($num == 1){
-    echo "found same email";
+    echo "email already exists";
 }
 // CREATE NEW MEMBER
 else{
+    //header('location:fitness.html#homepage');
+    echo "Creating Member... <br>";
+
+    // QUERY STRING FOR MEMBER
+    $createMember = "INSERT INTO prac (`FirstName`, `LastName`, `Email`, `Password`) VALUES ('".$first."','".$last."','".$email."','".$pwd."')";
+
+    // CREATE MEMBER
+    if ($mysqli->query($createMember) === TRUE) {
+        echo "New record created successfully";
+    } else {// RETURN ERROR
+        echo "Error: " . $createMember . "<br>" . $mysqli->error;
+    }
+
+
     
+    $mysqli->close();
+
 }
 ?>
