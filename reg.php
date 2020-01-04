f4593bc54b34520b2b4b5484736f7c98ac3ed0d4
Matthew Fischer <mattfisc@gmail.com>
2020-01-04 02:09:20 -0800

    php page 2 not working on registration

--- reg.php
+++ reg.php
@@ -9,6 +9,7 @@ $pwd= $_POST['pwd'];
 
 //NOTE: $mysqli = new mysqli("127.0.0.1", "username", "password", "database", 3306);
 $mysqli = new mysqli("localhost","root","","cs222");
+
 if ($mysqli->connect_errno) {
   echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
